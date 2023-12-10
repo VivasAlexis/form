4b9691157d703c0e8f4d427e573211f700949061
ReyVivas <118857031+VivasAlexis@users.noreply.github.com>
2023-12-10 12:10:20 -0500

    Add files via upload

---
+++ src/Main/Form.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Basic_Form.FormLabel;
+
+public class Form {
+
+    public static void main(String[] args) {
+        FormLabel formlabel = new FormLabel();
+        formlabel.setVisible(true);
+                
+    }
+    
+}
